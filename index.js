82399e3c040b626eb21ffc6bc40f4f2b0d0289b8
Mezan2002 <mezanurr009@gmail.com>
2022-11-24 12:31:58 +0600

    dotenv require done and db user and pass set on .env now time to get the DB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // requires start
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 // requires end
