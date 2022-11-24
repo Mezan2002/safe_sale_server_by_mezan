c143a2a21bc4b3c64151f1b2569a98c2e7c5d1ca
Mezan2002 <mezanurr009@gmail.com>
2022-11-24 12:50:53 +0600

    Gettign all categories by MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 // requires end
@@ -12,6 +13,40 @@ app.use(express.json());
 // middlewears end
 
 // express initial setup start
+
+// MongoDB connect API start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ahck7i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// collections making start
+
+const categoriesCollection = client.db("safeSale").collection("categories");
+
+// collections making end
+
+// CRUD's run function start
+const run = async () => {
+  try {
+    // get all categories API start
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.send(categories);
+    });
+    // get all categories API end
+  } finally {
+    // console.log();
+  }
+};
+run().catch((err) => console.error(err));
+// CRUD's run function end
+
+// MongoDB connect API end
 
 // default page API start
 app.get("/", (req, res) => {
